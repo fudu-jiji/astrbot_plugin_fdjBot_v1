c37d963c8fbcb9d2feb6f569f592407de324ce1d
fudu-jiji <53884348@qq.com>
2025-02-06 21:07:38 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
+from astrbot.api.all import * 
 
-@register("helloworld", "Your Name", "一个简单的 Hello World 插件", "1.0.0", "repo url")
-class MyPlugin(Star):
+@register("astrbot_plugin_fdjBot", "fuduji", "复读姬机器人第一代", "1.0.0", "https://github.com/fudu-jiji/astrbot_plugin_fdjBot_v1")
+class fdjPlugin(Star):
     def __init__(self, context: Context):
         super().__init__(context)
     
-    # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("helloworld")
+    # 注册指令
+    @filter.command("最新活动")
     async def helloworld(self, event: AstrMessageEvent):
-        '''这是一个 hello world 指令''' # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
+        '''最新的活动消息''' # 描述
         user_name = event.get_sender_name()
-        yield event.plain_result(f"Hello, {user_name}!") # 发送一条纯文本消息
+        yield event.plain_result(f"最近有2025年的第一个预售,包括了一个新春大礼包和碧蓝的春节系列卡片和DC最后三期! \n本期预售预计3月发货!") # 发送一条纯文本消息
